1fc342a4a713b88d1d4ed3904e3e8c0de4da4371
yazanhamzeh1 <144042660+yazanhamzeh1@users.noreply.github.com>
2025-02-16 20:10:53 -0500

    fixing button bounce issue by introducing debounce delay

--- button_debouncing/button_debouncing.ino
+++ button_debouncing/button_debouncing.ino
@@ -1,18 +1,31 @@
 #define BUTTON_PIN 7
 byte buttonState,readState; 
-  
+unsigned long lastTimeButtonChanged, debounceDelay,timeNow; 
 void setup() {
   buttonState = LOW;
   Serial.begin(115200);
   pinMode(BUTTON_PIN,INPUT);
+  debounceDelay = 10;
+  lastTimeButtonChanged = millis();
 
 }
 
 void loop() {
-  readState=digitalRead(BUTTON_PIN);
-  if(readState !=buttonState ){
-    buttonState = readState;
-    Serial.println(buttonState);
+  timeNow = millis();
+  // only read value every debounceTime
+  if(timeNow - lastTimeButtonChanged   > debounceDelay){
+    readState=digitalRead(BUTTON_PIN);
+    if(readState !=buttonState ){
+      lastTimeButtonChanged = timeNow;
+      buttonState = readState;
+      if(buttonState == LOW)
+        Serial.println("Button Release");
+      else
+        Serial.println("Button Pressed");
+
   }
+
+  }
+  
 
 }
